2d580d7b5cf5308affaa0f6e6a52f2f3492308b8
bliepp <comedyclub333.yt@googlemail.com>
2020-12-01 23:57:55 +0100

    Tabs in echo

--- semester_1/worksheet_02/solutions/measure_ex_3_5.sh
+++ semester_1/worksheet_02/solutions/measure_ex_3_5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 fn=outfiles/ex_3_5.out
-echo "#n\tt" > $fn
+echo -e "#n\tt" > $fn
 for i in {3..13}; do
 	echo "Calculating for $i*$i = $(($i*$i)) particles"
 	python ex_3_5.py $i >> $fn
