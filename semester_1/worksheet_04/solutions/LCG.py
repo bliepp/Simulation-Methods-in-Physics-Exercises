2ecad841efb9cc6918dd95c97ae7f9139eabd4bc
bliepp <comedyclub333.yt@googlemail.com>
2021-01-08 15:47:14 +0100

    Cleanup LCG again

--- semester_1/worksheet_04/solutions/LCG.py
+++ semester_1/worksheet_04/solutions/LCG.py
@@ -1,46 +1,44 @@
+import os, time
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 # bad random Number generator
-def LCG(seed = 0):
+def LCG(seed=0):
     a = 1103515245
     c = 12345
-    m = 1 << 32 # 2**32, bit shifted magic
-    random = seed
+    m = 1 << 32 # 2**32, bitshifting magic
+    current = seed
     while True:
-        random = (a * random + c) % m
-        yield random/m
+        current = (a * current + c) % m
+        yield current/m
 
 
 # random Walk
-def walk(N):
-    x = 0
-    y = 0
+def random_walk(N, seed=0):
+    rng = LCG(seed)
+    steps, positions = [0], [0]
 
-    steps = [x]
-    positions = [y]
-    
-    for i in range (1,N+1):
-        x += 1
-        y +=  LCG(1337) - 0.5
-
-        steps.append(x)
-        positions.append(y)
+    for i in range (N):
+        steps.append(steps[-1] + 1)
+        positions.append(positions[-1] + next(rng) - 0.5)
 
     return [steps, positions]
 
 
-def plot(data):
-    plt.figure(0)
-    plt.plot(dat[0],dat[1],markersize='0',linestyle='solid')
-    plt.xlabel(r'Steps')
-    plt.ylabel(r'Position')
-    plt.grid(alpha=0.7,linestyle=":")
-    #plt.legend(markerfirst=True,shadow=True)
-    plt.tight_layout()
-    plt.savefig("RWalk.pdf",format="pdf")
-    plt.show()
+def plot(filename):
+    with PdfPages(filename) as pdf:
+        plt.xlabel(r'Steps')
+        plt.ylabel(r'Position')
+        plt.grid(alpha=0.7,linestyle=":")
+
+        for name, seed in {"Fixed": 1337, "Time": time.time(), "PID": os.getpid()}.items():
+            data = random_walk(1000, seed)
+            plt.plot(data[0],data[1], label=name + " {}".format(seed), markersize='0', linestyle='solid')
+
+        plt.legend(markerfirst=True)
+        plt.tight_layout()
+        pdf.savefig()
+        plt.close()
 
 if __name__ == "__main__":
-    plot(walk(1000))
-
-
+    plot("RWalk.pdf")
