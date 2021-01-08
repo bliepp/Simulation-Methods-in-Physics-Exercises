db21fef17a84102ec3e358e1b0ba11fbb4f4bd74
bliepp <comedyclub333.yt@googlemail.com>
2021-01-08 14:36:13 +0100

    Clean up LCG

--- semester_1/worksheet_04/solutions/LCG.py
+++ semester_1/worksheet_04/solutions/LCG.py
@@ -1,50 +1,46 @@
 import matplotlib.pyplot as plt
 
-#Bad random Number generator
-def LCG():
+# bad random Number generator
+def LCG(seed = 0):
     a = 1103515245
     c = 12345
-    m = 2*10**(32)
-    LCG.random = (a * LCG.random + c) % m
-    return LCG.random/m
+    m = 1 << 32 # 2**32, bit shifted magic
+    random = seed
+    while True:
+        random = (a * random + c) % m
+        yield random/m
 
-LCG.random=1337
 
-#Test Generator
-#for i in range(10):
-#    print(LCG())
-
-#Random Walk
-
-def Walk(N):
+# random Walk
+def walk(N):
     x = 0
     y = 0
 
-    Step = [x]
-    position = [y]
+    steps = [x]
+    positions = [y]
     
     for i in range (1,N+1):
-        y +=  LCG() - 0.5
         x += 1
+        y +=  LCG(1337) - 0.5
 
-        Step.append(x)
-        position.append(y)
+        steps.append(x)
+        positions.append(y)
 
-    return[Step,position]
+    return [steps, positions]
 
-dat = Walk(1000)
 
-def plot():
-        plt.figure(0)
-        plt.plot(dat[0],dat[1],markersize='0',linestyle='solid')
-        plt.xlabel(r'Steps')
-        plt.ylabel(r'Position')
-        plt.grid(alpha=0.7,linestyle=":")
-        #plt.legend(markerfirst=True,shadow=True)
-        plt.tight_layout()
-        plt.savefig("RWalk.pdf",format="pdf")
-        plt.show()
-    
-plot()
+def plot(data):
+    plt.figure(0)
+    plt.plot(dat[0],dat[1],markersize='0',linestyle='solid')
+    plt.xlabel(r'Steps')
+    plt.ylabel(r'Position')
+    plt.grid(alpha=0.7,linestyle=":")
+    #plt.legend(markerfirst=True,shadow=True)
+    plt.tight_layout()
+    plt.savefig("RWalk.pdf",format="pdf")
+    plt.show()
+
+if __name__ == "__main__":
+    plot(walk(1000))
 
 
