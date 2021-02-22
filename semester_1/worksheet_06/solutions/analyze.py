368f586b865cf09cbd16c4cc3f8f7fc061a8c69c
bliepp <comedyclub333.yt@googlemail.com>
2021-02-22 15:39:51 +0100

    Analyze.py load files

--- semester_1/worksheet_06/solutions/analyze.py
+++ semester_1/worksheet_06/solutions/analyze.py
@@ -1,5 +1,13 @@
+import gzip
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def load_data(path):
+    with gzip.open(path, "rb") as f:
+        return pickle.load(f)
+    return dict()
 
 
 if __name__ == "__main__":
